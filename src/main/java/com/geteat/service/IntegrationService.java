c9a348f4a33847b04918ea6ea23dbc6abad69885
Jakub Chrzanowski <jl.chrzanowski@gmail.com>
2017-05-13 23:43:49 +0200

    News filtration #1

--- src/main/java/com/geteat/service/IntegrationService.java
+++ src/main/java/com/geteat/service/IntegrationService.java
@@ -1,11 +1,14 @@
 package com.geteat.service;
 
 import com.geteat.dao.UserDao;
+import com.geteat.dto.rso.News;
 import com.geteat.dto.rso.Rso;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Kuba on 2017-05-13.
@@ -27,7 +30,12 @@ public class IntegrationService {
 
     public void fetchLatestRso() {
         List allStates = userDao.findAllStates();
-        Rso latestNewsForStates = rsoService.getLatestNewsForStates(allStates);
+        Long latestId = 0L; //TODO implement getting it from base
+        List<News> latestNewsForStates = rsoService.getLatestNewsForStates(allStates, latestId);
+        Optional<News> max = latestNewsForStates
+                .stream()
+                .max((o1, o2) -> Long.compare(Long.valueOf(o1.getId()), Long.valueOf(o2.getId())));
+        latestNewsForStates.getClass();
         //TODO send rso to mail service
     }
 }
--- src/main/java/com/geteat/service/RsoService.java
+++ src/main/java/com/geteat/service/RsoService.java
@@ -1,11 +1,14 @@
 package com.geteat.service;
 
+import com.geteat.dto.rso.News;
 import com.geteat.dto.rso.Newses;
 import com.geteat.dto.rso.Rso;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Kuba on 2017-05-13.
@@ -15,11 +18,15 @@ public class RsoService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public Rso getLatestNewsForStates(List allStates) {
+    public List<News> getLatestNewsForStates(List allStates, Long latestId) {
         //TODO support pagination
-        return new Rso(restTemplate
+        Rso rso = new Rso(restTemplate
                 .getForObject("https://komunikaty.tvp.pl/komunikatyxml/wszystkie/wszystkie/1?_format=xml",
                         Newses.class));
+        return Arrays.stream(rso.getNewses().getNews())
+                .filter(news -> Long.valueOf(news.getId()) > latestId)
+                .filter(news -> allStates.contains(news.getProvinces().getProvince().getSlug()))
+                .collect(Collectors.toList());
     }
 
 
